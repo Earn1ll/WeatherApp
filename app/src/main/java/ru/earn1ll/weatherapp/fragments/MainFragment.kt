a7341c9f77f11d382262774f480479f83693746a
Earn1ll <coldsteelblr@gmail.com>
2024-02-24 13:14:16 +0300

    refactor: refactor function parsWeatherData

--- app/src/main/java/ru/earn1ll/weatherapp/fragments/MainFragment.kt
+++ app/src/main/java/ru/earn1ll/weatherapp/fragments/MainFragment.kt
@@ -97,15 +97,46 @@ class MainFragment : Fragment() {
 
     private fun parsWeatherData(result: String) {
         val mainObject = JSONObject(result)
+        val list =parseDays(mainObject)
+        parsCurrentWeatherData(mainObject,list[0])
+    }
+
+    private fun parseDays(mainObject: JSONObject):List<WeatherModel> {
+        val list = ArrayList<WeatherModel>()
+        val daysArray = mainObject.getJSONObject("forecast").getJSONArray("forecastday")
+        val name = mainObject.getJSONObject("location").getString("name")
+        val region = mainObject.getJSONObject("location").getString("region")
+        val country = mainObject.getJSONObject("location").getString("country")
+
+        for (i in 0 until daysArray.length()) {
+            val day = daysArray[i] as JSONObject
+            val item = WeatherModel(
+                name,
+                day.getString("date"),
+                day.getJSONObject("day").getJSONObject("condition").getString("text"),
+                "",
+                day.getJSONObject("day").getString("maxtemp_c"),
+                day.getJSONObject("day").getString("mintemp_c"),
+                day.getJSONObject("day").getJSONObject("condition").getString("icon"),
+                day.getJSONArray("hour").toString(),
+                region,
+                country
+            )
+            list.add(item)
+        }
+        return list
+    }
+
+    private fun parsCurrentWeatherData(mainObject: JSONObject,weatherItem:WeatherModel) {
         val item = WeatherModel(
             mainObject.getJSONObject("location").getString("name"),
             mainObject.getJSONObject("current").getString("last_updated"),
             mainObject.getJSONObject("current").getJSONObject("condition").getString("text"),
             mainObject.getJSONObject("current").getString("temp_c"),
-            "",
-            "",
+            weatherItem.maxTemperature,
+            weatherItem.minTemperature,
             mainObject.getJSONObject("current").getJSONObject("condition").getString("icon"),
-            "",
+            weatherItem.hours,
             mainObject.getJSONObject("location").getString("region"),
             mainObject.getJSONObject("location").getString("country"),
         )
@@ -117,6 +148,7 @@ class MainFragment : Fragment() {
         Log.d("myLog", "Region: ${item.region}")
         Log.d("myLog", "Country: ${item.country}")
     }
+
 
     companion object {
 
